cdb962a3737772f4fa52f12b6d6b60cad3ad0907
Mehedi Hasan メヘディ ハサン <mehedihasan.cse.jp@gmail.com>
2020-04-02 09:43:02 +0600

    Upload

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main()=>runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Log in screen",
+      home: LogInPage(),
+    );
+  }
+}
+class LogInPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
